971e718b21e545b80a0add9b887c3d4a56cb82cb
Umut Özkardeş <103787380+umutozkrds@users.noreply.github.com>
2025-06-30 15:09:17 +0300

    Update user routes: add endpoint for fetching all requests and ensure consistent formatting in request handling routes.

--- backend-Event/backend/router/users.js
+++ backend-Event/backend/router/users.js
@@ -8,6 +8,7 @@ router.post('/favourites/:eventId', checkAuth, userController.addFavourite);
 router.get('/favourites/:userId', checkAuth, userController.getFavourites);
 router.delete('/favourites/:eventId', checkAuth, userController.removeFavourite);
 router.get('/role/:userId', checkAuth, userController.getUserRole)
-router.post('/request/:userId',userController.makeRequest);
+router.post('/request/:userId', userController.makeRequest);
+router.get('/requests', userController.getRequests);
 
 module.exports = router;
