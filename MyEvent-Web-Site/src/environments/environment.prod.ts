8e619db6cf76a500365174f124289da024bf67c2
Umut Özkardeş <103787380+umutozkrds@users.noreply.github.com>
2025-07-03 17:10:26 +0300

    Update production environment configuration: change apiUrl to point to the production server and add adminEmail and adminId for backend integration.

--- MyEvent-Web-Site/src/environments/environment.prod.ts
+++ MyEvent-Web-Site/src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000/api',
+    apiUrl: 'http://188.132.197.87:3000/api',
+    adminEmail: 'admin@gmail.com',
+    adminId: '6866664b93cf81c1a77bc862',
     appName: 'MyEvent',
     version: '1.0.0'
 }; 
